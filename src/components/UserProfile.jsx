00d0d65bd10fd2fe733d40c0b725011729913770
Maria Naveen <marianaveen15@outlook.com>
2024-08-21 23:49:16 +0530

    Completed the user profile task

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+const UserProfile = () => {
+  const [details, setDetails] = useState({});
+  const [showBio, setShowBio] = useState(false);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setDetails(data);
+      });
+  }, []);
+  const handleClick = () => {
+    setShowBio(!showBio);
+  };
+  return (
+    <div className="mw-1/5 mx-auto p-10 border border-gray-700 shadow-2xl">
+      <p className="m-2">
+        <span className="font-bold text-lg">Name:</span> {details.name}
+      </p>
+      <p className="m-2">
+        <span className="font-bold text-lg">Email:</span> {details.email}
+      </p>
+      {showBio && (
+        <div className="m-2">
+          <span className="font-bold text-lg">Bio:</span>
+          <p>{`${details.address.street}, ${details.address.city}`}</p>
+          <p>{details.phone}</p>
+          <p>{details.website}d</p>
+          <p></p>
+        </div>
+      )}
+
+      <button
+        className="w-48 border text-white hover:text-gray-800 border-gray-700 bg-yellow-500 "
+        onClick={handleClick}
+      >
+        Show details
+      </button>
+    </div>
+  );
+};
+
+export default UserProfile;
